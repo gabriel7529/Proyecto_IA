7538d43f236a12f9de96075b46947741e7e497bd
Gabriel <gmachicaoq@unsa.edu.pe>
2024-07-25 20:39:44 -0500

    Poniendo anotaciones y generando el dataset

--- train_model.py
+++ train_model.py
@@ -25,4 +25,46 @@ def load_annotations(dir_path):
     annotations_df['file_path'] = annotations_df.apply(lambda row: os.path.join(dir_path, row['file_name']), axis=1)
     return annotations_df
 
+train_annotations = load_annotations(train_dir)
+valid_annotations = load_annotations(valid_dir)
+test_annotations = load_annotations(test_dir)
+
+def select_random_subset(df, sample_size):
+    return df.sample(n=sample_size, random_state=42)
+
+train_sample_size = 50000
+valid_sample_size = 5000
+test_sample_size = 1200
+
+train_annotations = select_random_subset(train_annotations, train_sample_size)
+valid_annotations = select_random_subset(valid_annotations, valid_sample_size)
+test_annotations = select_random_subset(test_annotations, test_sample_size)
+
+def load_image(img_path, label):
+    img = tf.keras.preprocessing.image.load_img(img_path, target_size=(224, 224))
+    img = tf.keras.preprocessing.image.img_to_array(img)
+    img = tf.keras.applications.resnet50.preprocess_input(img)
+    return img, label
+
+def data_generator(dataframe, batch_size, img_size):
+    while True:
+        for start in range(0, len(dataframe), batch_size):
+            end = min(start + batch_size, len(dataframe))
+            batch_data = dataframe[start:end]
+            images = []
+            labels = []
+            for idx, row in batch_data.iterrows():
+                img_path = row['file_path']
+                label = 1 if row['label'] == 'space-occupied' else 0
+                img, label = load_image(img_path, label)
+                images.append(img)
+                labels.append(label)
+            yield np.array(images), np.array(labels)
+
+batch_size = 32
+train_gen = data_generator(train_annotations, batch_size, (224, 224))
+valid_gen = data_generator(valid_annotations, batch_size, (224, 224))
+test_gen = data_generator(test_annotations, batch_size, (224, 224))
+
+
 
